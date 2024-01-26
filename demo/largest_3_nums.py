f49e2b8399c10b7643d094d7914ae6d3d2dab717
srikanthpragada <srikanthpragada@gmail.com>
2024-01-26 18:57:01 +0530

    26-JAN

---
+++ demo/largest_3_nums.py
@@ -0,0 +1,10 @@
+# print largest of 3 numbers
+
+a, b, c = 20, 15, 10
+
+if a > b and a > c:
+    print(a)
+elif b > c:
+    print(b)
+else:
+    print(c)
---
+++ demo/print_1_10.py
@@ -0,0 +1,4 @@
+# print 1 to 10
+
+for n in range(10, 0, -1):
+    print(n, end=' ')
